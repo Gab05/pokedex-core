b6411d4449ea46446e222edb3690697087e94b36
Gabriel Laliberté <gabriel.laliberte@ticketmaster.com>
2019-06-17 12:25:13 -0400

    moves endpoint fixed

--- src/main/kotlin/com/gablalib/pokedexcore/controllers/MoveController.kt
+++ src/main/kotlin/com/gablalib/pokedexcore/controllers/MoveController.kt
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("/moves")
 object MoveController {
 
-    @GetMapping("/")
+    @GetMapping("")
     fun moves() = MoveService.getAllMoves()
 
     @GetMapping("/{name}")
