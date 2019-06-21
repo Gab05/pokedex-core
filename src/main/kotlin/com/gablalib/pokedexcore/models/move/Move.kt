38d4454c047e966517694fb8cd415859ef3062f1
Gabriel Laliberté <gabriel.laliberte@ticketmaster.com>
2019-06-20 23:22:38 -0400

    Move.PP field refactor

--- src/main/kotlin/com/gablalib/pokedexcore/models/move/Move.kt
+++ src/main/kotlin/com/gablalib/pokedexcore/models/move/Move.kt
@@ -12,7 +12,7 @@ data class Move(val name: String,
                 val battleEffectRatio: Float = 0f,
                 val overworldEffect: String = String(),
                 val baseCriticalHitRatio: Float = 4.17f,
-                val maxPP: Int = 40,
+                val PP: Int = 40,
                 val category: MoveCategory = MoveCategory.OTHER,
                 val priority: Int = 0,
                 val flags: ArrayList<MoveFlag> = ArrayList())
