493a15921bbf96f8d9d681b579b8024673dcd326
Gabriel Laliberté <gabriel.laliberte@ticketmaster.com>
2019-08-04 18:16:37 -0400

    added support for pokemon egg moves and abilities

--- src/main/kotlin/com/gablalib/pokedexcore/models/pokemon/ability/Abilities.kt
+++ src/main/kotlin/com/gablalib/pokedexcore/models/pokemon/ability/Abilities.kt
@@ -1,6 +1,6 @@
 package com.gablalib.pokedexcore.models.pokemon.ability
 
-class Abilities(val first: Ability = Ability(),
-                val second: Ability = Ability(),
-                val hidden: Ability = Ability()
+class Abilities(val first: String = String(),
+                val second: String = String(),
+                val hidden: String = String()
 )
