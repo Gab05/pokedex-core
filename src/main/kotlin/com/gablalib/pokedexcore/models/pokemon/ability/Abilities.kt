7bb4ff857fbc61bc6a69bc9724e2d4370ff80f49
Gabriel Laliberté <gabriel.laliberte@ticketmaster.com>
2019-08-04 22:16:30 -0400

    tests

--- src/main/kotlin/com/gablalib/pokedexcore/models/pokemon/ability/Abilities.kt
+++ src/main/kotlin/com/gablalib/pokedexcore/models/pokemon/ability/Abilities.kt
@@ -2,5 +2,13 @@ package com.gablalib.pokedexcore.models.pokemon.ability
 
 class Abilities(val first: String = String(),
                 val second: String = String(),
-                val hidden: String = String()
-)
+                val hidden: String = String()) {
+
+    override fun equals(other: Any?): Boolean {
+        return if (other is Abilities) {
+            this.first.equals(other.first)
+                    && this.second.equals(other.second)
+                    && this.hidden.equals(other.hidden)
+        } else false
+    }
+}
--- src/main/kotlin/com/gablalib/pokedexcore/models/pokemon/gender/GenderRatio.kt
+++ src/main/kotlin/com/gablalib/pokedexcore/models/pokemon/gender/GenderRatio.kt
@@ -1,3 +1,11 @@
 package com.gablalib.pokedexcore.models.pokemon.gender
 
-class GenderRatio(val male: String = "50", val female: String = "50")
+class GenderRatio(val male: String = "50",
+                  val female: String = "50") {
+    override fun equals(other: Any?): Boolean {
+        return if (other is GenderRatio) {
+            this.male.equals(other.male) &&
+            this.female.equals(other.female)
+        } else false
+    }
+}
