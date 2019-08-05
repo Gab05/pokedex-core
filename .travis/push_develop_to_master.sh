4f11e459c82398d9a8efb4ccc618aa7a29d809d7
Gabriel Laliberté <gabriel.laliberte@ticketmaster.com>
2019-08-04 23:00:06 -0400

    still testing lol

--- .travis/push_develop_to_master.sh
+++ .travis/push_develop_to_master.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 
-git fetch origin
+cd ..
+git clone --branch=master https://github.com/Gab05/pokedex-core.git Gab05/pokedex-core-copy
+cd Gab05/pokedex-core-copy
+
+git config --replace-all remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
+git fetch
+
+# create the tacking branches
+for branch in $(git branch -r|grep -v HEAD) ; do
+    git checkout -qf ${branch#origin/}
+done
+
 git checkout master
-git merge origin develop
+git merge --squash origin develop
 git push origin master
+cd ..
