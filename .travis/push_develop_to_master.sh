818dbf116b14ffaae475fb877511efb4a7e27a95
Gabriel Laliberté <gabriel.laliberte@ticketmaster.com>
2019-08-05 13:00:50 -0400

    try cloning with https but pushing with ssh

--- .travis/push_develop_to_master.sh
+++ .travis/push_develop_to_master.sh
@@ -3,7 +3,7 @@
 cd ..
 
 # The filename of the key is the one specified in the ./decrypt_private_key.sh script
-ssh-agent bash -c 'ssh-add ./github_deploy_key; git clone --branch=master git@github.com:Gab05/pokedex-core.git Gab05/pokedex-core-copy'
+ssh-agent bash -c 'ssh-add ./github_deploy_key; git clone --branch=master https://github.com/Gab05/pokedex-core.git Gab05/pokedex-core-copy'
 
 cd Gab05/pokedex-core-copy
 
