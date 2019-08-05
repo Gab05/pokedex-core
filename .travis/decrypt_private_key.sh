df66aa587db986950744eb70db7029da786c1348
Gabriel Laliberté <gabriel.laliberte@ticketmaster.com>
2019-08-05 12:45:22 -0400

    still trying

--- .travis/decrypt_private_key.sh
+++ .travis/decrypt_private_key.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 openssl aes-256-cbc -K $encrypted_4bcfaff873d9_key -iv $encrypted_4bcfaff873d9_iv -in github_deploy_key.enc -out github_deploy_key -d
+ls
--- .travis/push_develop_to_master.sh
+++ .travis/push_develop_to_master.sh
@@ -3,9 +3,8 @@
 cd ..
 
 # The filename of the key is the one specified in the ./decrypt_private_key.sh script
-ssh-agent bash -c 'ssh-add ./github_deploy_key'
+ssh-agent bash -c 'ssh-add ./github_deploy_key; git clone --branch=master git@github.com:Gab05/pokedex-core.git Gab05/pokedex-core-copy'
 
-git clone --branch=master git@github.com:Gab05/pokedex-core.git Gab05/pokedex-core-copy
 cd Gab05/pokedex-core-copy
 
 git config --replace-all remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
