dfc2183a80c028cc7523985b7b14d6429bdddc50
Gabriel Laliberté <gabriel.laliberte@ticketmaster.com>
2019-08-05 13:19:30 -0400

    trying something else lol

--- .travis/decrypt_private_key.sh
+++ .travis/decrypt_private_key.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+echo "Decrypting private key..."
 openssl aes-256-cbc -K $encrypted_4bcfaff873d9_key -iv $encrypted_4bcfaff873d9_iv -in github_deploy_key.enc -out github_deploy_key -d
 chmod 400 github_deploy_key
+echo "Done!"
+
+echo "Moving key to ssh folder..."
 mv github_deploy_key ~/.ssh/
+echo "Done!"
--- .travis/push_develop_to_master.sh
+++ .travis/push_develop_to_master.sh
@@ -1,22 +1,29 @@
 #!/usr/bin/env bash
 
+echo "Cloning git repo..."
 cd ..
 
 # The filename of the key is the one specified in the ./decrypt_private_key.sh script
-ssh-agent bash -c 'ssh-add ./github_deploy_key; git clone --branch=master https://github.com/Gab05/pokedex-core.git Gab05/pokedex-core-copy'
+ssh-agent bash -c 'ssh-add ~/.ssh/github_deploy_key; git clone --branch=master https://github.com/Gab05/pokedex-core.git Gab05/pokedex-core-copy'
 
 cd Gab05/pokedex-core-copy
 
+echo "Fetching remote branches..."
 git config --replace-all remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
 git fetch
 
+echo "Create tracking branches..."
 # create the tacking branches
 for branch in $(git branch -r|grep -v HEAD) ; do
     git checkout -qf ${branch#origin/}
 done
 
+echo "Checkout master branch and merge from origin/develop..."
 git checkout master
 git merge --squash origin develop
+
+echo "Push to origin/master..."
 git push origin master
 
 cd ..
+echo "Done! develop merged into master."
